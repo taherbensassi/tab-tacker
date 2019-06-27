e1898216d2344752deb4fab70213d5db2b32ec08
taherbensassi <taher.sassi.isamm@gmail.com>
2019-06-27 22:31:21 +0200

    adding first url

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,33 @@
+/** reuired express**/
+const express = require ('express');
+/** reuired process jsondata easyly**/
+const bodyParser = require ('body-parser');
+/** reuired cors**/
+const cors = require ('cors');
+/** reuired cors**/
+const morgan = require ('morgan');
+
+/** web server build **/
+const app = express ();
+
+/** Enable packages **/
+/**-_-_-_-_-_-_-_-__-_**/
 
 
-console.log("hallo taher ben sassi");
+/**  printout log  **/
+app.use(morgan('combine'));
+/** Bodyparser allow you to parse json data very easy **/
+app.use(bodyParser.json());
+/** any client to access **/
+app.use(cors());
+
+
+app.get('/status', (req,res) => {
+    res.send({
+        message : "hallo world"
+    })
+});
+
+
+app.listen(process.env.PORT || 5000);
+
